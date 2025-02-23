b658607f92ca22ca70cb14b713991d8866b837cd
Gal4azz1 <gabrielegaleazzi07@gmail.com>
2025-02-23 18:44:31 +0100

    finalCommit

---
+++ static/script/index.js
@@ -0,0 +1,10 @@
+let button = document.querySelector('#button');
+button.addEventListener('click', function(){
+    let catergory = document.querySelector('.categories').value;
+    fetch('https://api.chucknorris.io/jokes/random?category=' + catergory)
+    .then(response => response.json())
+    .then(data => {
+        let joke = document.querySelector('#joke');
+        joke.innerHTML = data.value;
+    })
+})
